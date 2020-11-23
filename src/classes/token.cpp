dda09aa39d3c91c79c8decf37f78b1d7c790960d
ruben4447 <rubensaunders@hotmail.co.uk>
2020-11-23 18:19:36 +0000

    Initial Push

--- src/classes/token.cpp
+++
@@ -1,8 +0,0 @@
-#include <string>
-#include <vector>
-
-namespace bnf {
-    // { value, is_var }
-    typedef std::pair<std::string, bool> token_t;
-    typedef std::vector<token_t> tokenlist_t;
-};
